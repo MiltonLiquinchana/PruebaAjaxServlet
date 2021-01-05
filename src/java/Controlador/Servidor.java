3ca432a6041dc10e824ab416ffed40a7f9e28b1b
MiltonLQ <MiltonLQ@localhost>
2021-01-04 19:31:42 -0500

    se agrego comentarios a las lineas de codigo

--- src/java/Controlador/Servidor.java
+++ src/java/Controlador/Servidor.java
@@ -29,10 +29,10 @@ import org.json.simple.JSONObject;
  */
 public class Servidor extends HttpServlet {
 
-    Conexion conexion;
-    DAOtablaprueba tabla;
-    tablaprueba tabl;
-
+    /*no tomar en cuenta*/
+//    Conexion conexion;
+//    DAOtablaprueba tabla;
+//    tablaprueba tabl;
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -73,60 +73,14 @@ public class Servidor extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String id = request.getParameter("id");
-        System.out.println(id);
+        String id = request.getParameter("id");//capturo el dato enviado desde fetch el request.getParamete("") es equivalente a $_POST[""] de php
+        PrintWriter out = response.getWriter();
+        JSONObject json = new JSONObject();
+        json.put("ID server", id);
+        out.print(json);//imprimo(pero me da nulo)
 
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//    private void metodo() {
-//        //Creamos un objeto de tipo prinwriter esto para poder imprimir el json, desde el servlet
-//        PrintWriter out = null;
-//        try {
-//            JSONObject json;
-//            //leemos el fk
-//            String fk = request.getParameter("id");
-//            //System.out.println(fk);
-//            out = response.getWriter();
-//            tabla = new DAOtablaprueba();
-//            tabl = new tablaprueba();
-//            tabl = tabla.consultar(Integer.parseInt(fk));
-//            //llenamos un objeto de tipo json object
-//            json = new JSONObject();
-//            //agregamos propiedades al json
-//            json.put("id", tabl.getPk_tabla());
-//            json.put("codigo", tabl.getCodigo());
-//            out.print(json);
-//            json.clear();
-//
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(null, ex.getMessage());
-//        } finally {
-//            out.close();
-//        }
-//    }
     @Override
     public String getServletInfo() {
         return "Short description";
